cdcd2a24169c08d6cfa7eb8daffe95951b76dfc4
Vader1970 <daniel.wilkey@gmail.com>
2025-07-23 18:03:56 +1200

    first commit

---
+++ app/routes/home.tsx
@@ -0,0 +1,37 @@
+import type { MetaFunction } from "react-router";
+import { resumes } from "../../constants";
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
+
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
+  ];
+}
+
+export default function Home() {
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+
+
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1>Track Your Application & Resume</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+      
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+</section>
+    </main>
+  );
+}
